60eab1ce4b6a639e22483872d91cc1f8e53e41ea
Arturo Garcia <arturo.garcia@nucleodf.com>
2017-06-01 12:32:36 +0200

    20170601. Cambiado el modulo de traduccion...
    Signed-off-by: Arturo Garcia <arturo.garcia@nucleodf.com>

--- public/translator/lang.js
+++ public/translator/lang.js
@@ -2,6 +2,8 @@ var langs = ['en', 'es'];
 var langCode = '';
 var langJS = null;
 
+/** */
+var dictionary = [];
 
 var translate = function (jsdata)
 {	
@@ -15,19 +17,37 @@ var translate = function (jsdata)
 var translateWord = function (key,translate){
 	var strTr = '';
 	var lang = navigator.languages[0].substr (0, 2);
-	if (langs.indexOf(lang)>=0){
-		$.getJSON('lang/'+lang+'.json', function(result){
-		    translate(result[key]);
-		});
+	if (langs.indexOf(lang)>=0) {
+		/** 20170529 AGL. Para que no se pida continuamente el fichero de traduccion */
+		if (dictionary.length == 0) {
+			$.getJSON('lang/'+lang+'.json', function(result){
+				dictionary = result;
+			    translate(result[key]);
+			});
+		}
+		else {
+			translate(dictionary[key]);
+		}
+		/**************************************/
 	}
 }
 
 var translateForm = function (){
 	//langCode = navigator.language.substr (0, 2);
+	console.log("translateForm");
 	langCode = navigator.languages[0].substr (0, 2);
 
-	if (langs.indexOf(langCode)>=0)
+	/** 20170529. AGL. Para que no se pida continuamente el fichero de traduccion */
+	var langFile = langs.indexOf(langCode)>=0 ? ('lang/'+langCode+'.json') : 'lang/en.json';
+	if (dictionary.length == 0) {
+		$.getJSON(langFile, function(result) {
+			dictionary = result;
+			translate(result);
+		});
+	}
+	/*if (langs.indexOf(langCode)>=0)
 		$.getJSON('lang/'+langCode+'.json', translate);
 	else
 		$.getJSON('lang/en.json', translate);
+	*************/
 }
