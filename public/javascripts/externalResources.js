5bdb75420a05b598283b67db7c9826e0365b9cd4
Vicente Molina Garcia <vmolinagarcia@gmail.com>
2017-09-13 13:47:04 +0200

    Fichero propio javascript

---
+++ public/javascripts/externalResources.js
@@ -0,0 +1,38 @@
+/**
+ * Created by vjmolina on 13/9/17.
+ */
+
+/************************************/
+/*	FUNCTION: GetTablesBss	 		*/
+/*  PARAMS: 						*/
+/*  REV 1.0.2 VMG					*/
+/************************************/
+var GetExtResources = function(f) {
+	/** 20170511 AGL PERFILES */
+	if (Authorize($('#BodyRedan').data('perfil'),[ccAdminProfMsc,ccConfiProfMsc])==false) {
+		/*if ((($('#BodyRedan').data('perfil') & 16) != 16) && (($('#BodyRedan').data('perfil') & 64) != 64)) {
+		 *****************************/
+		alertify.error('No tiene asignados permisos para la gestión de las tablas de calificación de audio.');
+		return;
+	}
+	
+	$('#AddResources').hide();
+	$("#FormResources").show();
+	$('#DivResources').animate({width: '535px'});
+	
+	/*$.ajax({type: 'GET',
+	 url: '/tableBss'})
+	 .done(function(data){
+	 $("#listTablesBss").empty();
+	 if (data.tables != null && data.tables.length > 0){
+	 $.each(data.tables, function(index, value){
+	 var item = $("<li><a onclick='GetTable(" + value.idtabla_bss + ")'>" + value.name + "</li>");
+	 item.appendTo($("#listTablesBss"));
+	 });
+	 if (f != null)
+	 f();
+	 }
+	 else if (f != null)
+	 f();
+	 });*/
+};
